aa94dae936e1254d78945f9e4904f4c758325b1d
gnatal <guilhermenatal47@gmail.com>
2022-07-31 11:23:44 -0300

    About initial setups and deleting old stuff

---
+++ src/components/About/Contact/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <p>Reach us by this number: 9 9999 9999</p>
+    </div>
+  );
+}
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,17 +3,10 @@ import Banner from '../Banner';
 
 export default function About() {
   return (
-    <Banner>
-      <div className="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50">
-        <div className="text-center">
-          <h1 className="text-white text-2xl font-semibold uppercase md:text-3xl">
-            Build Your new
-            {' '}
-            <span className="underline text-blue-400">Saas</span>
-          </h1>
-          <button type="button" className="mt-4 px-4 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Start project</button>
-        </div>
+    <div>
+      <div>
+        <p>Just a about</p>
       </div>
-    </Banner>
+    </div>
   );
 }
